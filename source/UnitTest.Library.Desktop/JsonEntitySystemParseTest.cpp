31b38dd358819c5a9185c2768192281fdaf72fd9
Logan.Harvell <Logan.Harvell@ucf.edu>
2020-03-25 00:57:41 -0500

    Entity System: changed Sector and World parse tests to have arrays of Entities and Sectors respectively
    [git-p4: depot-paths = "//depot/Students/CH16/Logan.Harvell/Prog2/fieagameengine/": change = 19743]

--- source/UnitTest.Library.Desktop/JsonEntitySystemParseTest.cpp
+++ source/UnitTest.Library.Desktop/JsonEntitySystemParseTest.cpp
@@ -102,18 +102,36 @@ namespace UnitTestLibraryDesktop
 				  "type": "string",
 				  "value": "Sector"
 				},
-				"Entities": { 
+				"Entities": {
 				  "type": "Entity",
-				  "value": {
-				      "Name": {
-				        "type": "string",
-				        "value": "Entity1"
-				      },
-				      "AuxiliaryInt": {
-				        "type": "integer",
-				        "value": 10
+				  "value": [
+				    {
+				      "type": "Entity",
+				      "value": {
+				        "Name": {
+				          "type": "string",
+				          "value": "Entity1"
+				        },
+				        "AuxiliaryInt": {
+				          "type": "integer",
+				          "value": 10
+				        }
 				      }
-				  }
+				    },
+				    {
+				      "type": "Entity",
+				      "value": {
+				        "Name": {
+				          "type": "string",
+				          "value": "Entity2"
+				        },
+				        "AuxiliaryInt": {
+				          "type": "integer",
+				          "value": 20
+				        }
+				      }
+				    }
+				  ]
 				}
 			})";
 
@@ -121,18 +139,24 @@ namespace UnitTestLibraryDesktop
 
 			Assert::AreEqual(3_z, sector.Size());
 			Assert::AreEqual("Sector"s, sector.Name());
-			Assert::AreEqual(1_z, sector.Entities().Size());
+			Assert::AreEqual(2_z, sector.Entities().Size());
 
 			Assert::IsNotNull(sector.Find("Entities"));
 			Assert::AreEqual(sector.Entities(), *sector.Find("Entities"));
 
 			Entity* entity1 = sector.Entities().Get<Scope*>(0)->As<Entity>();
+			Entity* entity2 = sector.Entities().Get<Scope*>(1)->As<Entity>();
 
 			Assert::IsNotNull(entity1);
+			Assert::IsNotNull(entity2);
 
 			Assert::AreEqual("Entity1"s, entity1->Name());
 			Assert::IsNotNull(entity1->Find("AuxiliaryInt"));
 			Assert::AreEqual(10, entity1->Find(("AuxiliaryInt"))->Get<int>());
+
+			Assert::AreEqual("Entity2"s, entity2->Name());
+			Assert::IsNotNull(entity2->Find("AuxiliaryInt"));
+			Assert::AreEqual(20, entity2->Find(("AuxiliaryInt"))->Get<int>());
 		}
 
 		TEST_METHOD(ParseWorld)
@@ -148,56 +172,61 @@ namespace UnitTestLibraryDesktop
 			parser.AddHelper(helper);
 			parser.SetSharedData(sharedData);
 
-			std::string json = R"({
-				"Name": {
-				  "type": "string",
-				  "value": "World"
-				},
-				"Sectors": {
-				  "type": "Sector",
-				  "value": {
-				    "Name": {
-				      "type": "string",
-				      "value": "Sector1"
-				    },
-				    "Entities": {
-				      "type": "Entity",
-				      "value": {
-				        "Name": {
-				          "type": "string",
-				          "value": "Entity1"
-				        },
-				        "AuxiliaryInt": {
-				          "type": "integer",
-				          "value": 10
-				        }
-				      }
-				    }
-				  }
-				}
-			})";
-
-			parser.Parse(json);
+			parser.ParseFromFile("Content/World.json");
 
 			Assert::AreEqual(3_z, world.Size());
 			Assert::AreEqual("World"s, world.Name());
-			Assert::AreEqual(1_z, world.Sectors().Size());
+			Assert::AreEqual(2_z, world.Sectors().Size());
 
-			Sector* sector = world.Sectors().Get<Scope*>()->As<Sector>();
-			Assert::IsNotNull(sector);
+			/* Sector 1 */
 
-			Assert::AreEqual("Sector1"s, sector->Name());
+			Sector* sector1 = world.Sectors().Get<Scope*>(0)->As<Sector>();
+			Assert::IsNotNull(sector1);
 
-			Assert::IsNotNull(sector->Find("Entities"));
-			Assert::AreEqual(sector->Entities(), *sector->Find("Entities"));
+			Assert::AreEqual("Sector1"s, sector1->Name());
 
-			Entity* entity1 = sector->Entities().Get<Scope*>(0)->As<Entity>();
+			Assert::IsNotNull(sector1->Find("Entities"));
+			Assert::AreEqual(sector1->Entities(), *sector1->Find("Entities"));
+			Assert::AreEqual(2_z, sector1->Entities().Size());
 
+			Entity* entity1 = sector1->Entities().Get<Scope*>(0)->As<Entity>();
+			Entity* entity2 = sector1->Entities().Get<Scope*>(1)->As<Entity>();
+			
 			Assert::IsNotNull(entity1);
+			Assert::IsNotNull(entity2);
 
 			Assert::AreEqual("Entity1"s, entity1->Name());
 			Assert::IsNotNull(entity1->Find("AuxiliaryInt"));
 			Assert::AreEqual(10, entity1->Find(("AuxiliaryInt"))->Get<int>());
+
+			Assert::AreEqual("Entity2"s, entity2->Name());
+			Assert::IsNotNull(entity2->Find("AuxiliaryInt"));
+			Assert::AreEqual(20, entity2->Find(("AuxiliaryInt"))->Get<int>());
+
+			/* Sector 2*/
+
+			Sector* sector2 = world.Sectors().Get<Scope*>(1)->As<Sector>();
+			Assert::IsNotNull(sector2);
+			
+			Assert::AreEqual("Sector2"s, sector2->Name());
+			
+			Assert::IsNotNull(sector2->Find("Entities"));
+			Assert::AreEqual(sector2->Entities(), *sector2->Find("Entities"));
+			Assert::AreEqual(2_z, sector2->Entities().Size());
+
+			entity1 = sector2->Entities().Get<Scope*>(0)->As<Entity>();
+			entity2 = sector2->Entities().Get<Scope*>(1)->As<Entity>();
+
+			Assert::IsNotNull(entity1);
+			Assert::IsNotNull(entity2);
+
+			Assert::AreEqual("Entity1"s, entity1->Name());
+			Assert::IsNotNull(entity1->Find("AuxiliaryInt"));
+			Assert::AreEqual(10, entity1->Find(("AuxiliaryInt"))->Get<int>());
+
+			Assert::AreEqual("Entity2"s, entity2->Name());
+			Assert::IsNotNull(entity2->Find("AuxiliaryInt"));
+			Assert::AreEqual(20, entity2->Find(("AuxiliaryInt"))->Get<int>());
 		}
 
 	private:
