96c21c8ae2f6ac6693c45832c8919b0e54ea8c6c
Logan.Harvell <Logan.Harvell@ucf.edu>
2020-03-24 22:32:05 -0500

    Added missing FooEntity class
    [git-p4: depot-paths = "//depot/Students/CH16/Logan.Harvell/Prog2/fieagameengine/": change = 19735]

---
+++ source/UnitTest.Library.Desktop/FooEntity.cpp
@@ -0,0 +1,49 @@
+#include "pch.h"
+#include "FooEntity.h"
+
+namespace UnitTests
+{
+	const Library::TypeManager::TypeInfo& FooEntity::TypeInfo()
+	{
+		static const Library::TypeManager::TypeInfo typeInfo
+		{
+			{
+				{ "Data", Types::Integer, false, 1, offsetof(FooEntity, mData) }
+			},
+
+			Entity::TypeIdClass()
+		};
+
+		return typeInfo;
+	}
+
+	FooEntity::FooEntity() : Entity(TypeIdClass())
+	{
+	}
+
+	gsl::owner<Library::Scope*> FooEntity::Clone() const
+	{
+		return new FooEntity(*this);
+	}
+
+	void FooEntity::Update(Library::WorldState& worldState)
+	{
+		Entity::Update(worldState);
+
+		worldState.Entity = this;
+		if (!mIsUpdated) mIsUpdated = true;
+		worldState.Entity = nullptr;
+	}
+
+	std::string FooEntity::ToString() const
+	{
+		std::ostringstream oss;
+		oss << Name() << " (FooEntity)";
+		return oss.str();
+	}
+	
+	bool FooEntity::IsUpdated()
+	{
+		return mIsUpdated;
+	}
+}
