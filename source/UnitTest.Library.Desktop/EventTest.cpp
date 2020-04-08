2c9e682550d07700c95ecb238d80fa61ff5ea13a
Logan.Harvell <Logan.Harvell@ucf.edu>
2020-04-07 21:31:03 -0500

    Events: added missing tests
    [git-p4: depot-paths = "//depot/Students/CH16/Logan.Harvell/Prog2/fieagameengine/": change = 20716]

---
+++ source/UnitTest.Library.Desktop/EventTest.cpp
@@ -0,0 +1,150 @@
+#include "pch.h"
+
+#include "ToStringSpecialization.h"
+#include "Event.h"
+#include "IEventSubscriber.h"
+#include "EventQueue.h"
+
+using namespace std::string_literals;
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace UnitTests;
+
+namespace EventTests
+{
+	class TestEventSubscriber final : public Foo, public IEventSubscriber
+	{
+	public:
+		virtual void Notify(EventPublisher& eventPublisher) override
+		{
+			Data() = static_cast<Event<Foo>&>(eventPublisher).Message().Data();
+		}
+	};
+
+
+	TEST_CLASS(EventTest)
+	{
+	public:
+		TEST_METHOD_INITIALIZE(Initialize)
+		{
+#if defined(DEBUG) || defined(_DEBUG)
+			_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF);
+			_CrtMemCheckpoint(&sStartMemState);
+#endif
+		}
+
+		TEST_METHOD_CLEANUP(Cleanup)
+		{
+
+#if defined(DEBUG) || defined(_DEBUG)
+			_CrtMemState endMemState, diffMemState;
+			_CrtMemCheckpoint(&endMemState);
+			if (_CrtMemDifference(&diffMemState, &sStartMemState, &endMemState))
+			{
+				_CrtMemDumpStatistics(&diffMemState);
+				Assert::Fail(L"Memory Leaks!");
+			}
+#endif
+		}
+
+		TEST_METHOD(Subscription)
+		{
+			TestEventSubscriber testEventSubscriber1;
+			TestEventSubscriber testEventSubscriber2;
+			TestEventSubscriber testEventSubscriber3;
+
+			Assert::AreEqual(0_z, Event<Foo>::SubscriberCount());
+			Assert::AreEqual(0_z, Event<Foo>::SubscriberCapacity());
+
+			Event<Foo>::Subscribe(testEventSubscriber1);
+			Assert::AreEqual(1_z, Event<Foo>::SubscriberCount());
+			Assert::AreNotEqual(0_z, Event<Foo>::SubscriberCapacity());
+
+			Assert::ExpectException<std::runtime_error>([&] 
+			{
+				Event<Foo>::Subscribe(testEventSubscriber1); 
+			});
+
+			Event<Foo>::Subscribe(testEventSubscriber2);
+			Assert::AreEqual(2_z, Event<Foo>::SubscriberCount());
+			Event<Foo>::Subscribe(testEventSubscriber3);
+			Assert::AreEqual(3_z, Event<Foo>::SubscriberCount());
+
+			std::size_t capacity = Event<Foo>::SubscriberCapacity();
+
+			Event<Foo>::Unsubscribe(testEventSubscriber1);
+			Assert::AreEqual(2_z, Event<Foo>::SubscriberCount());
+			Assert::AreEqual(capacity, Event<Foo>::SubscriberCapacity());
+
+			Event<Foo>::UnsubscribeAll();
+			Assert::AreEqual(0_z, Event<Foo>::SubscriberCount());
+			Assert::AreEqual(capacity, Event<Foo>::SubscriberCapacity());
+
+			Event<Foo>::SubscriberShrinkToFit();
+			Assert::AreEqual(0_z, Event<Foo>::SubscriberCapacity());
+		}
+
+		TEST_METHOD(Constructor)
+		{
+			{
+				Event<Foo> fooEvent;
+				Assert::AreEqual(Foo(), fooEvent.Message());
+			}
+
+			Event<Foo> fooEvent(Foo(10));
+			Assert::AreEqual(Foo(10), fooEvent.Message());
+
+			const Event<Foo> constFooEvent = fooEvent;
+			Assert::AreEqual(Foo(10), constFooEvent.Message());
+		}
+
+		TEST_METHOD(Publish)
+		{
+			Event<Foo> fooEvent(Foo(10));
+			TestEventSubscriber subscriber;
+			Event<Foo>::Subscribe(subscriber);
+
+			Assert::AreEqual(0, subscriber.Data());
+			fooEvent.Publish();
+			Assert::AreEqual(fooEvent.Message().Data(), subscriber.Data());
+
+			Event<Foo>::UnsubscribeAll();
+			Event<Foo>::SubscriberShrinkToFit();
+		}
+
+		TEST_METHOD(RTTITest)
+		{
+			Event<Foo> a;
+			Event<Foo> b;
+
+			Assert::IsTrue(a.Is(EventPublisher::TypeIdClass()));
+			Assert::IsTrue(a.Equals(&b));
+
+			EventPublisher* fooEvent = new Event<Foo>();
+			bool isEvent = fooEvent->Is(Event<Foo>::TypeIdClass());
+
+			Event<Foo>* createdFooEvent = isEvent ? createdFooEvent = fooEvent->CreateAs<Event<Foo>>() : nullptr;
+			bool wasCreated = createdFooEvent != nullptr;
+
+			bool isFooEvent = wasCreated ? createdFooEvent->Is(Event<Foo>::TypeIdClass()) : false;
+
+			delete fooEvent;
+			delete createdFooEvent;
+
+			Assert::IsTrue(isEvent);
+			Assert::IsTrue(wasCreated);
+			Assert::IsTrue(isFooEvent);
+		}
+
+		TEST_METHOD(ToString)
+		{
+			Event<Foo> fooEvent;
+			Assert::AreEqual("Event"s, fooEvent.ToString());
+			Assert::AreEqual("EventPublisher"s, fooEvent.EventPublisher::ToString());
+		}
+
+	private:
+		static _CrtMemState sStartMemState;
+	};
+
+	_CrtMemState EventTest::sStartMemState;
+}
