ce8aa7739fda19d46e1c222c165aa9e2ec02beeb
Logan.Harvell <Logan.Harvell@ucf.edu>
2020-06-17 18:27:19 -0500

    deleting branch
    [git-p4: depot-paths = "//depot/Students/CH16/Logan.Harvell/Prog2/SolarSystemDemo/": change = 24020]

--- content/Shaders/DepthMapVS.hlsl
+++
@@ -1,9 +0,0 @@
-cbuffer CBufferPerObject
-{
-	float4x4 WorldLightViewProjection;
-}
-
-float4 main(float4 ObjectPosition : POSITION) : SV_Position
-{
-	return mul(ObjectPosition, WorldLightViewProjection);
-}
--- source/Library.Desktop/Content/Shaders/BasicPS.hlsl
+++
@@ -1,9 +0,0 @@
-cbuffer CBufferPerObject
-{
-	float4 SurfaceColor;
-}
-
-float4 main(float4 position : SV_Position) : SV_TARGET
-{
-	return SurfaceColor;
-}
